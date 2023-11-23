71b7470fd71c7c12c4def222c8f1e8d32a976d10
Sab-Ben <sbenseghir.sb@gmail.com>
2023-11-23 12:42:52 +0100

    Initialisation du projet

---
+++ chat-websocket-backend/src/main/java/com/chat/websocket/backend/ChatWebsocketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chat.websocket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebsocketBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWebsocketBackendApplication.class, args);
+    }
+
+}
